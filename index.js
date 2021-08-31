c15b2e2070367b92dad4c6280471b32ad12865db
Abhishek Sahani <abhisheksahani653@gmail.com>
2021-08-31 14:02:26 +0530

    Done

---
+++ index.js
@@ -0,0 +1,30 @@
+var intialPrice = document.querySelector("#initial-price");
+var stocksQuantity = document.querySelector("#stocks-quantity");
+var currentPrice = document.querySelector("#current-price");
+var submitButton = document.querySelector("#submit-btn");
+var result = document.querySelector("#output-box");
+
+submitButton.addEventListener("click", clickHandler );
+
+function clickHandler() {
+  result.style.display = "none";
+
+  var initial = intialPrice.value;
+  var quantity = stocksQuantity.value;
+  var current = currentPrice.value;
+
+  if (initial > current) {
+    var loss = (initial - current) * quantity ;
+    var lossPercentage = (loss / initial) * 100 ;
+
+    result.style.display = "block";
+    result.innerText = `Hey, the loss is ${loss} and the percentage is ${lossPercentage}%` ;
+
+  }else if (current > initial) {
+    var profit = (current - initial) * quantity ;
+    var profitPercentage = (profit / initial) * 100 ;
+
+    result.style.display = "block";
+    result.innerText = `Hey, the profit is ${profit} and the percentage is ${profitPercentage}%` ;
+  }
+}
