15fe2f07e3d30409cd2eff530aa8d72c72afb8a4
Abhishek Sahani <abhisheksahani653@gmail.com>
2021-09-03 21:08:18 +0530

    Bug fix

--- index.js
+++ index.js
@@ -13,18 +13,24 @@ function clickHandler() {
   var quantity = stocksQuantity.value;
   var current = currentPrice.value;
 
-  if (initial > current) {
-    var loss = (initial - current) * quantity ;
-    var lossPercentage = (loss / initial) * 100 ;
+  if (initial > 0 && quantity > 0 && current > 0) {
 
+    if (initial > current) {
+      var loss = (initial - current) * quantity ;
+      var lossPercentage = (loss / initial) * 100 ;
+
+      result.style.display = "block";
+      result.innerText = `Hey, the loss is ${loss} and the percentage is ${lossPercentage}%` ;
+
+    }else if (current > initial) {
+      var profit = (current - initial) * quantity ;
+      var profitPercentage = (profit / initial) * 100 ;
+
+      result.style.display = "block";
+      result.innerText = `Hey, the profit is ${profit} and the percentage is ${profitPercentage}%` ;
+    }
+  }else {
     result.style.display = "block";
-    result.innerText = `Hey, the loss is ${loss} and the percentage is ${lossPercentage}%` ;
-
-  }else if (current > initial) {
-    var profit = (current - initial) * quantity ;
-    var profitPercentage = (profit / initial) * 100 ;
-
-    result.style.display = "block";
-    result.innerText = `Hey, the profit is ${profit} and the percentage is ${profitPercentage}%` ;
+    result.innerText = "Enter positive number" ;
   }
 }
